c03c47ea3509411c40ed9218995e0f8ca0d58803
Vitaliy Yemets <vitali8.emez@gmail.com>
2023-11-04 14:14:30 +0200

    initial commit

---
+++ src/main/kotlin/com/ever/everhackback/EverHackBackApplication.kt
@@ -0,0 +1,11 @@
+package com.ever.everhackback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EverHackBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<EverHackBackApplication>(*args)
+}
---
+++ src/test/kotlin/com/ever/everhackback/EverHackBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ever.everhackback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EverHackBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
