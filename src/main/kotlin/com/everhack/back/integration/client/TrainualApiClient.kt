f21993b58f1ebdb1b70cf03ce3ce7f938b085736
Vitaliy Yemets <vitali8.emez@gmail.com>
2023-11-04 16:14:12 +0200

    remove feign; add datasource configs

--- src/main/kotlin/com/everhack/back/integration/client/TrainualApiClient.kt
+++
@@ -1,7 +0,0 @@
-package com.everhack.back.integration.client
-
-import org.springframework.cloud.openfeign.FeignClient
-
-@FeignClient(name = "trainual-api", url = "\${application.trainual-url}")
-interface TrainualApiClient {
-}
