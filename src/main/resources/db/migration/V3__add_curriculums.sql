ac63b0159c5a81c71f06422d0bac8c1a95445a65
Andrii Vedmid <andrii.vedmid@sigma.software>
2023-11-05 12:02:44 +0200

    Add SQL Curriculum

---
+++ src/main/resources/db/migration/V3__add_curriculums.sql
@@ -0,0 +1,6 @@
+INSERT INTO curriculums(role_id, name, description)
+VALUES (6, 'Program for Java Middle Developer ', 'This program is for Middle developer positions ');
+
+INSERT INTO curriculums(role_id, name, description)
+VALUES (5, 'Program for Java Senior Developer ', 'This program is for Senior developer positions ');
+
