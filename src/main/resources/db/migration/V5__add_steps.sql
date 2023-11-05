478e48f63f77707a37ec33be7a298af0f9fb4a07
Andrii Vedmid <andrii.vedmid@sigma.software>
2023-11-05 12:29:14 +0200

    Add SQL Curriculum

---
+++ src/main/resources/db/migration/V5__add_steps.sql
@@ -0,0 +1,59 @@
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (1, 'step 1');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (1, 'step 2');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (2, 'step 1');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (2, 'step 2');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (3, 'step 1');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (3, 'step 2');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (4, 'step 1');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (4, 'step 2');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (5, 'step 1');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (5, 'step 2');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (6, 'step 1');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (6, 'step 2');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (7, 'step 1');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (7, 'step 2');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (8, 'step 1');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (8, 'step 2');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (9, 'step 1');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (9, 'step 2');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (10, 'step 1');
+
+INSERT INTO course_steps(COURSE_ID, TITLE)
+VALUES (10, 'step 2');
