af7cca7cde9902a6462e59eb960c9d4e8927c0b0
Andrii Vedmid <andrii.vedmid@sigma.software>
2023-11-05 12:25:11 +0200

    Add SQL Curriculum

---
+++ src/main/resources/db/migration/V4__add_curriculum_elements.sql
@@ -0,0 +1,29 @@
+INSERT INTO curriculum_elements(curriculum_id, type, position, status, title)
+VALUES (1, 'COURSE', 1, 'FINISHED', 'Java Core');
+
+INSERT INTO curriculum_elements(curriculum_id, type, position, status, title)
+VALUES (1, 'COURSE', 2, 'FINISHED', 'Spring Framework Fundamentals Course');
+
+INSERT INTO curriculum_elements(curriculum_id, type, position, status, title)
+VALUES (1, 'COURSE', 3, 'FINISHED', 'Database Management with SQL Course');
+
+INSERT INTO curriculum_elements(curriculum_id, type, position, status, title)
+VALUES (1, 'COURSE', 4, 'FINISHED', 'Version Control with Git Course');
+
+INSERT INTO curriculum_elements(curriculum_id, type, position, status, title)
+VALUES (1, 'SURVEY', 5, 'PENDING', 'TEST for position Java Middle Developer');
+
+INSERT INTO curriculum_elements(curriculum_id, type, position, status, title)
+VALUES (2, 'COURSE', 1, 'PENDING', 'Advanced Java Topics');
+
+INSERT INTO curriculum_elements(curriculum_id, type, position, status, title)
+VALUES (2, 'COURSE', 2, 'FINISHED', 'Spring Framework Fundamentals Course');
+
+INSERT INTO curriculum_elements(curriculum_id, type, position, status, title)
+VALUES (2, 'COURSE', 3, 'PENDING', 'Database Management with SQL Course');
+
+INSERT INTO curriculum_elements(curriculum_id, type, position, status, title)
+VALUES (2, 'COURSE', 4, 'PENDING', 'Version Control with Git Course');
+
+INSERT INTO curriculum_elements(curriculum_id, type, position, status, title)
+VALUES (2, 'SURVEY', 5, 'PENDING', 'TEST for position Java Middle Developer');
